875c615613ed51258d5cffffead6645b1ce66b4a
Ettore Di Giacinto <mudler@users.noreply.github.com>
2025-10-30 18:16:12 +0100

    Change iteration expectation to be greater than or equal

--- reviewer_e2e_test.go
+++ reviewer_e2e_test.go
@@ -67,7 +67,7 @@ var _ = Describe("cogito test", Label("e2e"), func() {
 			))
 			Expect(err).ToNot(HaveOccurred())
 			Expect(f.String()).ToNot(BeEmpty())
-			Expect(f.Status.Iterations).To(Equal(2))
+			Expect(f.Status.Iterations).To(BeNumerically(">=", 2))
 			Expect(len(f.Status.ToolsCalled)).To(BeNumerically(">=", 2))
 			Expect(f.Status.ToolsCalled[0].Tool().Function.Name).To(Equal("search"))
 			Expect(f.Status.ToolResults[0].Executed).To(BeTrue())
